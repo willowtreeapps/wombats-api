7c4613ea4f744b1e213819d035ce728b3027ed7e
JanxSpirit <greggcarrier@gmail.com>
2016-04-27 17:02:32 -0400

    stub out some arena creation functions

--- src/clj/battlebots/arena.clj
+++ src/clj/battlebots/arena.clj
@@ -2,9 +2,34 @@
 
 (def arena-key {:open " "
                 :bot "@"
-                :block "X"})
+                :block "X"
+                :food "+"
+                :poison "-"})
 
-(defn arena
+(defn empty-arena
   [dimx dimy]
-  (vec (replicate (vec (replicate (:open arena-key) dimy)) dimx)))
+  (vec (replicate dimx (vec (replicate dimy (:open arena-key))))))
+
+(defn blocks
+  "sprinkle blocks around the arena argument and return a new arena
+  make sure there are no inaccessible areas"
+  [arena]
+  arena)
+
+(defn food
+  "sprinkle food around the arena argument and return a new arena"
+  [arena]
+  arena)
+
+(defn poison
+  "sprinkle poison around the arena argument and return a new arena"
+  [arena]
+  arena)
+
+(defn new-arena
+  "compose all arena building functions to make a fresh new arena"
+  [dimx dimy]
+  ((comp poison food blocks empty-arena) dimx dimy))
+
+
 
