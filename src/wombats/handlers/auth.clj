6a24766cceee38a835ddea84b90d802d3a24896f
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-01-16 08:54:03 -0500

    finish github oauth signup / signin  workflow

--- src/wombats/handlers/auth.clj
+++ src/wombats/handlers/auth.clj
@@ -1,6 +1,8 @@
 (ns wombats.handlers.auth
   (:require [io.pedestal.interceptor.helpers :refer [defbefore]]
             [clojure.core.async :refer [chan go >!]]
+            [org.httpkit.client :as http]
+            [cheshire.core :refer [parse-string]]
             [wombats.interceptors.github :refer [get-github-settings]]))
 
 (def ^:private github-base "https://github.com/login/oauth/")
@@ -10,6 +12,26 @@
 (def ^:private user-repos-url "https://api.github.com/user/repos")
 
 (def ^:private github-scopes "user:email")
+
+(defn- get-access-token
+  "POST request to get a GitHub OAuth token.
+  If no auth token is found, nil will return"
+  [request-params]
+  (http/post github-access-token-url
+             request-params
+             (fn [{:keys [body]}]
+               (get (parse-string body true) :access_token nil))))
+
+(defn- redirect-home
+  "Final redirect home with or without access token appended"
+  ([context]
+   (assoc context :response (assoc (:response context)
+                                   :headers {"Location" "/"}
+                                   :status 302)))
+  ([context access-token]
+   (assoc context :response (assoc (:response context)
+                                   :headers {"Location" (str "/?access-token=" access-token)}
+                                   :status 302))))
 
 ;; TODO signing secret should not be consistant across requests. Gen uuid to send.
 (defbefore github-redirect
@@ -24,22 +46,22 @@
       (>! ch (assoc context :response (assoc response
                                              :headers {"Location" github-redirect}
                                              :status 302
-                                             :body ""))))
+                                             :body nil))))
     ch))
 
 (defbefore github-callback
   [{:keys [request response] :as context}]
   (let [ch (chan 1)
-        {:keys [client-id client-secret]} (get-github-settings context)
-        {:keys [code state]} (:query-params request)
-        github-access-token-endpoint (str github-access-token-url
-                                          "?client_id=" client-id
-                                          "&client_secret=" client-secret
-                                          "&code=" code)]
+        {:keys [client-id client-secret signing-secret]} (get-github-settings context)
+        {:keys [code state]} (:query-params request)]
     (go
-      ;; TODO Fetch auth code
-      (>! ch (assoc context :response (assoc response
-                                             :headers {"Content-Type" "text/html"}
-                                             :status 200
-                                             :body "Callback"))))
+      (if (= state signing-secret)
+        (let [access-token @(get-access-token {:query-params {:client_id client-id
+                                                              :client_secret client-secret
+                                                              :code code}
+                                               :headers {"Accept" "application/json"}})]
+          (if access-token
+            (>! ch (redirect-home context access-token))
+            (>! ch (redirect-home context))))
+        (>! ch (redirect-home context))))
     ch))
