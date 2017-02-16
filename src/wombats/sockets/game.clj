3c8a0b8047a54d64035dee57b6ee406edca4e2d8
Christian Paul Dehli <dehli@gatech.edu>
2017-02-16 12:00:44 -0500

    Send player stats to frontend.

--- src/wombats/sockets/game.clj
+++ src/wombats/sockets/game.clj
@@ -130,6 +130,42 @@
   [game-id chan-id]
   (:color (get-game-room-player game-id chan-id)))
 
+;; Player Stats Methods
+
+(defn- add-player-scores
+  [stats players]
+  (reduce
+   (fn [stats-acc [uuid {:keys [stats user wombat player]}]]
+     (let [score (:stats/score stats)
+           user (:user/github-username user)
+           wombat (:wombat/name wombat)
+           color (:player/color player)]
+       (assoc stats-acc uuid {:score score
+                              :username user
+                              :wombat-name wombat
+                              :color color})))
+   stats players))
+
+(defn- add-player-hp
+  [stats arena]
+  (let [player-ids (set (keys stats))]
+    (reduce
+     (fn [stats-acc cell]
+       (let [cell-uuid (get-in cell [:contents :uuid])]
+         (if (contains? player-ids cell-uuid)
+           (assoc-in stats-acc [cell-uuid :hp] (get-in cell [:contents :hp]))
+           stats-acc)))
+     stats (flatten arena))))
+
+(defn get-player-stats
+  "Returns stats from game-state"
+  [game-state]
+  (-> {}
+      (add-player-scores (:players game-state))
+      (add-player-hp (get-in game-state [:frame
+                                         :frame/arena]))
+      vals))
+
 ;; Broadcast helper functions
 
 (defn- broadcast-to-viewers
@@ -225,7 +261,8 @@
       ;; Sends the current game frame to the frontend
       ((send-arena arena) chan-id)
 
-      ;; TODO: Sends the players to the front end instead of fake data
+      ;; Sends the stats (player info)
+      ((send-stats (get-player-stats game-state)) chan-id)
 
 
       ;; Sends the game info to the front end
