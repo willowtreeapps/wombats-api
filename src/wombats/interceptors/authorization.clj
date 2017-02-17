b892aa43e7e6778da41de03c2d833dceba49b23b
Matt O'Connell <matthew.j.oconnell1@gmail.com>
2017-02-17 10:52:39 -0500

    add authorization interceptor and protect the routes

--- src/wombats/interceptors/authorization.clj
+++ src/wombats/interceptors/authorization.clj
@@ -1,4 +1,5 @@
-(ns wombats.interceptors.authorization)
+(ns wombats.interceptors.authorization
+  (:require [wombats.interceptors.current-user :refer [get-current-user]]))
 
 (defn add-security-settings
   "Adds the security settings map to context"
@@ -21,3 +22,55 @@
 (defn get-hashing-secret
   [context]
   (get-in context [::security-settings :signing-secret]))
+
+(defn- user-has-role?
+  "Checks to see if a user has a role contained in the passed in role set"
+  [user roles]
+  (let [user-roles (set (map :db/ident (:user/roles user)))]
+    (not (empty? (clojure.set/intersection user-roles roles)))))
+
+#_(defn- get-permissions
+  [context]
+  (get context ::permissions #{}))
+
+#_(def ^:private check-permissions
+  {:name ::check-permissions
+   :enter (fn [context]
+            (let [authorized-roles (get-permissions context)
+                  current-user (get-current-user context)
+                  _ (when-not current-user
+                      (authorization-error))]
+              (if-not (user-has-role? current-user authorized-roles)
+                (authorization-error)
+                context)))})
+
+#_(defn- ensure-check-permissions-interceptor
+  [context]
+  (enqueue context [check-permissions]))
+
+#_(defn- add-permissions
+  "Adds a set of permissions to a route"
+  [context permissions]
+  (assoc context ::permissions (clojure.set/union (get-permissions context)
+                                                  permissions)))
+
+#_(defn authorize
+  "Protects a route (and it's children) from unauthorized access"
+  [authorized-roles]
+  {:name ::authorize-user
+   :enter (fn [context]
+            (-> context
+                (ensure-check-permissions-interceptor)
+                (add-permissions authorized-roles)))})
+
+(defn authorize
+  "Protects a route (and it's children) from unauthorized access"
+  [authorized-roles]
+  {:name ::authorize-user
+   :enter (fn [context]
+            (let [current-user (get-current-user context)
+                  _ (when-not current-user
+                      (authorization-error))]
+              (if-not (user-has-role? current-user authorized-roles)
+                (authorization-error)
+                context)))})
