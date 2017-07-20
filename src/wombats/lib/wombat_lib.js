92154e640ebc4c22240421e8e1c75a536faf3476
Daniel-Hoerauf <dah4pe@virginia.edu>
2017-07-20 11:18:03 -0400

    Moved wombats lib to be tracked by git

---
+++ src/wombats/lib/wombat_lib.js
@@ -0,0 +1,3 @@
+// Have module.exports be a function so that user won't have to worry about namespaces
+module.exports = function() {
+};
---
+++ src/wombats/lib/wombat_lib.py
@@ -0,0 +1,1 @@
+# TODO: Port standard library over from clojure
